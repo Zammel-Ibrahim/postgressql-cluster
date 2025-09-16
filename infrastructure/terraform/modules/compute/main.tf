f9264124c5882e6b6a35b93b620359ce944776ee
EC2 Default User <ec2-user@ip-172-31-22-47.ec2.internal>
2025-09-16 15:22:48 +0000

    commit 5

--- infrastructure/terraform/modules/compute/main.tf
+++ infrastructure/terraform/modules/compute/main.tf
@@ -8,31 +8,42 @@ resource "aws_security_group" "etcd_sg" {
     protocol        = "tcp"
     security_groups = [aws_security_group.pg_sg.id]
   }
-  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+  egress { 
+    from_port = 0 
+    to_port = 0 
+    protocol = "-1" 
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
 }
+
 
 resource "aws_security_group" "pg_sg" {
   name        = "${var.name_prefix}-pg-sg"
   vpc_id      = var.vpc_id
   description = "Patroni/Postgres"
   ingress {
+    security_groups = aws_security_group.pg_sg.id
     from_port       = 5432
     to_port         = 5432
     protocol        = "tcp"
-    security_groups = [aws_security_group.pg_sg.id]
   }
   ingress {
+    security_groups = aws_security_group.pg_sg.id
     from_port       = 9000
     to_port         = 9001
     protocol        = "tcp"
-    security_groups = [aws_security_group.pg_sg.id]
   }
-  egress { from_port = 0 to_port = 0 protocol = "-1" cidr_blocks = ["0.0.0.0/0"] }
+  egress {
+    from_port = 0 
+    to_port = 0 
+    protocol = "-1" 
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
 }
 
 resource "aws_instance" "etcd" {
   count                  = var.etcd_count
-  ami                    = "<YOUR_ETCD_AMI>"
+  ami                    = "ami-0b09ffb6d8b58ca91"
   instance_type          = var.instance_type
   key_name               = var.ssh_key_name
   vpc_security_group_ids = [aws_security_group.etcd_sg.id]
@@ -42,7 +53,7 @@ resource "aws_instance" "etcd" {
 
 resource "aws_instance" "pg" {
   count                  = var.pg_count
-  ami                    = "<YOUR_PG_18_AMI>"
+  ami                    = "ami-0b09ffb6d8b58ca91"
   instance_type          = var.instance_type
   key_name               = var.ssh_key_name
   vpc_security_group_ids = [aws_security_group.pg_sg.id]
--- infrastructure/terraform/modules/compute/variables.tf
+++ infrastructure/terraform/modules/compute/variables.tf
@@ -1,7 +1,39 @@
-output "etcd_private_ips" {
-  value = aws_instance.etcd[*].private_ip
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
 }
 
-output "pg_private_ips" {
-  value = aws_instance.pg[*].private_ip
+variable "public_subnets" {
+  description = "List of public subnet IDs"
+  type        = list(string)
+}
+
+variable "private_subnets" {
+  description = "List of private subnet IDs"
+  type        = list(string)
+}
+
+variable "etcd_count" {
+  description = "Number of etcd nodes to create"
+  type        = number
+}
+
+variable "pg_count" {
+  description = "Number of Postgres nodes to create"
+  type        = number
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "name_prefix" {
+  description = "Prefix for naming EC2 instances"
+  type        = string
+}
+
+variable "ssh_key_name" {
+  description = "SSH key pair name to assign"
+  type        = string
 }
